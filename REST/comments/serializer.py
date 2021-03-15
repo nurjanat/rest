893b504b5fc95231a942cfd356d6b0bd2090a7f2
nurjanat <tungatarovva@gmail.com>
2021-03-15 17:01:22 +0600

    serializerss

--- REST/comments/serializer.py
+++ REST/comments/serializer.py
@@ -1,8 +1,10 @@
-from rest_framework.fields import CharField
+from rest_framework.fields import CharField, SerializerMethodField
 
 from .models import Comment
 from rests.models import Book
 from rest_framework.serializers import ModelSerializer,Serializer
+from estimate.serializer import RateModelSerializer
+
 
 class CommentSerializer(ModelSerializer):
 
@@ -19,12 +21,23 @@ class CommentCreteSerializer(Serializer):
 
 
 class BookDetailSerializer(ModelSerializer):
-
+    rates = RateModelSerializer(many=True)
     comment_set = CommentSerializer(many=True)
-
+    avg_rate = SerializerMethodField()
     class Meta:
         model = Book
-        fields = ['id','title','description','price','year','author','comment_set']
+        fields = ['id','title','description','price','year','author','comment_set','rates','avg_rate']
+
+    def get_avg_rate(self,obj):
+        total_star = 0
+        for rate in obj.rates.all():
+            total_star += rate.star
+        return round(total_star/len(obj.rates.all()),1)
+
+
+
+
+
 
 
 
--- REST/estimate/serializer.py
+++ REST/estimate/serializer.py
@@ -3,3 +3,10 @@ from .models import Rate
 
 class RateSerializer(serializers.Serializer):
     star = serializers.IntegerField(min_value=0,max_value=10)
+
+
+class RateModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Rate
+        fields = ['id','star']
